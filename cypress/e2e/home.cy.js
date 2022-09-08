1f57d1da1b40d00c7eb9e9bbaa79b4616d332ffc
Javier Romero <javier.romero@instructure.com>
2022-09-07 22:52:08 -0600

    Adding test cases for Form Fields

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -8,21 +8,50 @@ describe('Home Page Tests', () => {
     cy.visit('https://www.instructure.com/es')
   })
 
-  it('It should navigate to the DEMO page', () => {
-    
-    cy.get('#block-getademo').contains('SOLICITAR UNA DEMO').click()
-    cy.url().should('include', 'request-demo')
-  })
-
   describe('Div block for Cookies policies', () => {
-    it('should Accept the Cookies for the website', () => {
-      cy.get('#onetrust-accept-btn-handler').contains('I Accept').click()
-      cy.get('#onetrust-accept-btn-handler').should('not.exist')
-    })
+  //   it('should Accept the Cookies for the website', () => {
+  //     cy.get('#onetrust-accept-btn-handler').contains('I Accept').click()
+  //     cy.get('#onetrust-accept-btn-handler').should('not.exist')
+  //   })
 
     it('should Accept the Cookies for the website', () => {
-      cy.get('#onetrust-policy-text').should('include', 'By continuing to browse or by clicking')
+      cy.get('#onetrust-banner-sdk').should('contain', 'By continuing to browse')
     })
   })
+
+  describe('Access the DEMO form', () => {
+  
+    it('It should navigate to the DEMO page and fill the form', () => {
+    
+      cy.get('#block-getademo').contains('SOLICITAR UNA DEMO').click()
+
+      cy.url().should('include', 'request-demo')
+
+      cy.get('#edit-first-name--2').type('Javier')
+
+      cy.get('#edit-last-name--2').type('Romero')
+
+      cy.get('#edit-email--2').type('javier@test.com')
+
+      cy.get('#edit-phone--2')
+
+      cy.get('#edit-country--2').select('El Salvador').should('have.value', 'El Salvador')
+
+      cy.get('#edit-job-title-es--2')
+
+      cy.get('#edit-organization--2')
+
+      cy.get('#edit-organization-type-es--2')
+
+      cy.get('#edit-fte-range-es--2')
+
+      cy.get('#edit-consent--2')
+
+      cy.get('#edit-submit--2').click()
+    })
+
+  })
+
+  
 
 })
