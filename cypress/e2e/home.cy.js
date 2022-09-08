725d3dabc9c7ab2e82ea698680563b3113084083
Javier Romero <javier.romero@instructure.com>
2022-09-07 21:46:42 -0600

    Removing test scripts not related to this training

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -8,12 +8,6 @@ describe('Home Page Tests', () => {
     cy.visit('https://www.instructure.com/es')
   })
 
-  it('Click on second page', () => {
-    
-    cy.get(':nth-child(1) > .css-1a0uvd3-view-rowHeader > .css-3orjyc-checkbox > .css-1h7tu9i-checkbox__control > .css-1v5k22r-checkboxFacade > .css-1dyk9la-checkboxFacade__facade').check()
-
-  })
-
   it('It should navigate to the DEMO page', () => {
     
     cy.get('#block-getademo').contains('SOLICITAR UNA DEMO').click()
@@ -21,19 +15,14 @@ describe('Home Page Tests', () => {
   })
 
   describe('Div block for Cookies policies', () => {
-    it('I should Accept the Cookies for the website', () => {
+    it('should Accept the Cookies for the website', () => {
       cy.get('#onetrust-accept-btn-handler').contains('I Accept').click()
       cy.get('#onetrust-accept-btn-handler').should('not.exist')
     })
 
-    it('I should Accept the Cookies for the website', () => {
+    it('should Accept the Cookies for the website', () => {
       cy.get('#onetrust-policy-text').should('include', 'By continuing to browse or by clicking')
     })
-  })
-  it('Select All items from the list', () => {
-    
-    cy.get(':nth-child(1) > .css-1epaw4q-view--inlineBlock-baseButton').click()
-
   })
 
 })
